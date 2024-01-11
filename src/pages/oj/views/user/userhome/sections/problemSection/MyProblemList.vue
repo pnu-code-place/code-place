2040f0ff90b49c510eabd2a96655a237f50e3050
Dongjin <ehdwls1638@gmail.com>
2024-01-11 11:03:05 +0900

    feat : problem 컴포넌트 레이아웃 추가

---
+++ src/pages/oj/views/user/userhome/sections/problemSection/MyProblemList.vue
@@ -0,0 +1,34 @@
+<script>
+
+import MyProblemItem from "./MyProblemItem.vue";
+
+export default {
+  components: {
+    MyProblemItem,
+  },
+  props: {
+    problems: {
+      type: Array,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    }
+  },
+}
+
+</script>
+
+<template>
+<div class="problem-list">
+  <h2>{{title}}</h2>
+  <div class="problem-items">
+    <my-problem-item v-for="problem in problems" :key="problem.id" :problem="problem" />
+  </div>
+</div>
+</template>
+
+<style scoped lang="less">
+
+</style>
