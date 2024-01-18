c5a9f9ba59b0f94d2bda84f9f5e0a1d8d9fb396f
Dongjin <ehdwls1638@gmail.com>
2024-01-18 22:33:09 +0900

    feat : 진행도 gauge 컴포넌트 추가

---
+++ src/pages/oj/views/user/userhome/sections/DashboardSection/HorizontalGauge.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  name : 'horizontal-gauge',
+  props: ['progress'],
+  computed: {
+    gaugeWidth() {
+      return `${this.progress * 100}%`;
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="horizontal-gauge">
+    <div class="gauge-background">
+
+    </div>
+    <div class="gauge" :style="{width : gaugeWidth}">
+
+    </div>
+  </div>
+</template>
+
+<style scoped lang="less">
+.horizontal-gauge {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+  display : flex;
+  justify-content : center;
+
+  .gauge-background {
+    width: 100%;
+    height: 80%;
+    background-color: #dedede;
+    position: absolute;
+    left: 0;
+    top: 10%;
+    border-radius: 20px;
+  }
+  .gauge {
+    height: 100%;
+    background-color: #22CC77;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-radius: 20px;
+  }
+}
+</style>
