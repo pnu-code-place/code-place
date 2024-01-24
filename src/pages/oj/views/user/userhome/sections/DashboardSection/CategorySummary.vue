7691c52241bf33aad921b47a3c787eba899e0070
Dongjin <ehdwls1638@gmail.com>
2024-01-25 00:20:28 +0900

    feat : category chart 기능 추가

--- src/pages/oj/views/user/userhome/sections/DashboardSection/CategorySummary.vue
+++ src/pages/oj/views/user/userhome/sections/DashboardSection/CategorySummary.vue
@@ -29,7 +29,7 @@
 // }
 export default {
   name: "category-summary",
-  props: ['category_info'],
+  props: ['categoryInfo'],
   data() {
     return {
       category_label: {
@@ -38,16 +38,92 @@ export default {
         sorting: '정렬',
         implementation: '구현',
         searching: '탐색'
-      }
+      },
     }
   },
-  methods: {}
+  methods: {
+    percentageToScoreElement(label, percentage) {
+      return `<div style="display:flex; width:100%; justify-content: space-between"><span>${label}</span><span>${((1 - percentage) * 100).toFixed(1)}</span></div>`
+    }
+  },
+  computed: {
+    graphRankData() {
+      return Object.keys(this.categoryInfo).map((key) => ((1 - this.categoryInfo[key].ranking_percent) * 100).toFixed(1))
+    },
+    tooltipFormatter() {
+      return `<div style="display:flex; flex-direction: column; padding:4px 15px">영역별 상대점수<br>
+              ${this.percentageToScoreElement('자료구조', this.categoryInfo.data_structure.ranking_percent)}
+              ${this.percentageToScoreElement('구현', this.categoryInfo.implementation.ranking_percent)}
+              ${this.percentageToScoreElement('수학', this.categoryInfo.mathematics.ranking_percent)}
+              ${this.percentageToScoreElement('탐색', this.categoryInfo.searching.ranking_percent)}
+              ${this.percentageToScoreElement('정렬', this.categoryInfo.sorting.ranking_percent)} </div>`
+    },
+    tooltip() {
+      return {
+        trigger: 'item',
+        formatter: this.tooltipFormatter,
+        backgroundColor: 'rgba(0,0,0,0.7)',
+        textStyle: {
+          color: '#ffffff',
+          fontWeight: 'bold'
+        }
+      }
+    },
+    chartOption() {
+      return {
+        color: ['#FF917C'],
+        legend: {},
+        tooltip: {
+          trigger: 'axis'
+        },
+        radar: {
+          indicator: [
+            {text: '자료구조', max: 100, color: '#000000',},
+            {text: '구현', max: 100, color: '#000000',},
+            {text: '수학', max: 100, color: '#000000',},
+            {text: '탐색', max: 100, color: '#000000',},
+            {text: '정렬', max: 100, color: '#000000',},
+          ],
+          axisName: {
+            fontWeight: 'bold'
+          },
+          splitArea: {
+            areaStyle: {
+              color: ['rgba(255,255,255,0)', 'rgba(255,255,255,0)', 'rgba(255,255,255,0)', 'rgba(255,255,255,0)'],
+            }
+          },
+          splitNumber: 4,
+          center: ['50%', '55%'],
+        },
+        series: [
+          {
+            name: 'ACM Score',
+            type: 'radar',
+            emphasis: {
+              areaStyle: {
+                color: '#FF917C',
+                shadowColor: '#FF917C'
+              },
+            },
+            data: [
+              {
+                value: this.graphRankData,
+                tooltip: this.tooltip,
+              },
+            ],
+          }
+        ]
+      }
+    }
+  }
 }
 </script>
 
 <template>
   <div class="category-summary">
-    <div class="graph-column">ㅇ</div>
+    <div class="graph-column">
+      <ECharts :options="chartOption" style="width: 100%; height: 100%"/>
+    </div>
     <div class="table-wrapper">
       <table>
         <thead>
@@ -58,7 +134,7 @@ export default {
         </tr>
         </thead>
         <tbody>
-        <tr class="part-row" v-for="(category, category_name, index) in category_info">
+        <tr class="part-row" v-for="(category, category_name, index) in categoryInfo">
           <td class="part-name">{{ category_label[category_name] }}</td>
           <td class="solve-number">{{ category.solve_number }}</td>
           <td class="solve-ranking">
@@ -84,7 +160,7 @@ export default {
   gap: 20px;
 
   .graph-column {
-    width: 25%;
+    width: 30%;
     height: 100%;
   }
 
@@ -96,12 +172,13 @@ export default {
       border-collapse: collapse;
 
       thead tr {
-        border-top : none;
+        border-top: none;
       }
 
       tr {
 
         border-top: 1px solid #dedede;
+
         th {
           text-align: center;
           font-size: 16px;
@@ -111,9 +188,11 @@ export default {
           width: 50%;
           text-align: left
         }
+
         th:nth-child(2) {
           width: 20%;
         }
+
         th:nth-child(3) {
           .ranking {
             font-weight: 700;
