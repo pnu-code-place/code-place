0c1d89cc824c9a21c4147619deedee00d7e9cd98
hunsy9 <juniper0917@gmail.com>
2024-02-28 12:58:46 +0900

    fix: college, department 더미데이터 수정

--- src/pages/oj/components/dropdown/test.js
+++ src/pages/oj/components/dropdown/test.js
@@ -1,5 +1,5 @@
-let collegeDto = [{id: 1, name: "공과대학"},{id: 2, name: "자연대학"},{id: 3, name: "의과대학"},{id: 4, name: "나노대학"}]
-let majorDto = [{id: 1, name: "정보컴퓨터공학전공"},{id: 2, name: "인공지능전공"},{id: 3, name: "기계공학"},{id: 4, name: "생명공학"}]
+let collegeDto = [{id: 1, name: "공과대학"}]
+let majorDto = [{id: 1, name: "정보컴퓨터공학전공"}]
 const DropDownType = {
   COLLEGE: "단과대학 선택",
   MAJOR: "학과 선택",
