964d37c71f8af44364fe8ee4757715ac00a5a2e4
hunsy9 <juniper0917@gmail.com>
2024-03-27 14:33:34 +0900

    chore: Linux 배포 테스트

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -58,7 +58,9 @@ cd $APP
 n=0
 while [ $n -lt 5 ]
 do
-    python manage.py migrate --no-input &&
+#    python manage.py migrate --no-input &&
+    python3 manage.py migrate contest 0011
+    python3 manage.py migrate
     python manage.py inituser --username=root --password=rootroot --action=create_super_admin &&
     echo "from options.options import SysOptions; SysOptions.judge_server_token='$JUDGE_SERVER_TOKEN'" | python manage.py shell &&
     echo "from conf.models import JudgeServer; JudgeServer.objects.update(task_number=0)" | python manage.py shell &&
