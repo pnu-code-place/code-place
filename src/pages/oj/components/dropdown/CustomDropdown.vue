db27f6833e54dc64dd3c5cfc15872cde33a2138e
minmunui <ehdwls1638@gmail.com>
2024-03-28 21:22:19 +0900

    #17 docs : dropdown 컴포넌트 주석 document 주석 추가

--- src/pages/oj/components/dropdown/CustomDropdown.vue
+++ src/pages/oj/components/dropdown/CustomDropdown.vue
@@ -1,4 +1,15 @@
+
 <script>
+/**
+ * CustomDropdown
+ * @description: CustomDropdown 컴포넌트는 select 태그를 사용하여 드롭다운을 구현한 컴포넌트입니다.
+ * @props options: 드롭다운에 표시할 항목들을 배열로 받습니다. 각 항목은 객체로 구성되어야 하며, nameKey와 valueKey를 통해 각 항목의 표시값과 실제 값이 저장된 key값을 지정할 수 있습니다.
+ * @props defaultValue: 드롭다운의 기본값을 설정합니다. 기본값이 없을 경우 -1로 설정합니다.
+ * @props defaultText: 드롭다운의 기본값이 없을 때 표시할 텍스트를 설정합니다. 설정하지 않으면 기본값이 표시되지 않습니다.
+ * @props nameKey: 드롭다운에 표시할 항목의 key값을 설정합니다.
+ * @props valueKey: 드롭다운에서 선택한 항목으로 넘겨줄 값의 key값을 설정합니다.
+ * @emits dropdownChange: 드롭다운에서 선택한 항목의 value값을 부모 컴포넌트로 전달합니다.
+ */
 export default {
   name: 'CustomDropdown',
   data() {
