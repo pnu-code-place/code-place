48c7620479b9e4a8d787d384e8c8e884ba2ed3a3
유승훈 <101303791+hunsy9@users.noreply.github.com>
2024-05-03 02:20:53 +0900

    Revert "#41 초기db세팅 마이그레이션 오류 (#42)"
    This reverts commit c7d1a759b1d1ddef17ffba3ad6022dc6c3d7ca0b.

--- submission/migrations/0007_auto_20170923_1318.py
+++ submission/migrations/0007_auto_20170923_1318.py
@@ -10,7 +10,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contest', '0011_auto_20231203_0742'),
         ('submission', '0006_auto_20170830_1154'),
     ]
 
