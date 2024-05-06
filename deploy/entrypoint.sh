d339f064765d0889ea891599633cbe594d132795
hunsy9 <juniper0917@gmail.com>
2024-05-07 00:15:15 +0900

    fix: entrypoint.sh loaddata 시작 전 sleep

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -80,6 +80,7 @@ done
 
 # 데이터베이스에 College와 Department 데이터가 이미 로드되었는지 확인
 if [ "$(echo "from account.models import College, Department; print(College.objects.count() == 0 or Department.objects.count() == 0)" | python manage.py shell)" == "True" ]; then
+    sleep 2
     python3 manage.py loaddata ./fixtures/*.json
 fi
 
