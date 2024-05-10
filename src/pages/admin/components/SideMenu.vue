fb505d680a68154ff3f82cf51879eaffa6321c36
llddang <77055208+llddang@users.noreply.github.com>
2024-05-10 11:46:57 +0900

    #97 admin 페이지의 logo 변경 (#98)
    * chore: 새로운 logo 추가
    
    #97
    
    * feat: logo를 불러오는 url 변경
    
    #97
    
    * refact: 사용되지 않는 css 요소 삭제
    
    #97

--- src/pages/admin/components/SideMenu.vue
+++ src/pages/admin/components/SideMenu.vue
@@ -2,7 +2,7 @@
   <el-menu class="vertical_menu"
            :router="true" :default-active="currentPath">
     <div class="logo">
-      <img src="../../../assets/logo.svg" alt="oj admin"/>
+      <img src="../../../assets/pnu_logo.svg" alt="oj admin"/>
     </div>
     <el-menu-item index="/"><i class="el-icon-fa-dashboard"></i>{{$t('m.Dashboard')}}</el-menu-item>
     <el-submenu v-if="isSuperAdmin" index="general">
@@ -61,9 +61,6 @@
       margin: 20px 0;
       text-align: center;
       img {
-        background-color: #fff;
-        border-radius: 50%;
-        border: 3px solid #fff;
         width: 75px;
         height: 75px;
       }
