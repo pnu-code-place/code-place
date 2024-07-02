d9732d266e0bf2214b7be5e356c3b5601393b839
minmunui <ehdwls1638@gmail.com>
2024-07-03 01:53:32 +0900

    #90 메인화면 공지사항 디자인 고도화

--- frontend/src/pages/oj/views/home/Notice/HomeNoticeBox.vue
+++ frontend/src/pages/oj/views/home/Notice/HomeNoticeBox.vue
@@ -2,98 +2,142 @@
   <div class="noticeBox">
     <div class="noticeBoxHeader">
       <span @click="handleRoute('notice')">공지사항</span>
-      <div class="plusDiv" @click="goAnnouncement('')">
-        <Icon type="android-add" size="13" color="#7a7a7a"></Icon>
-        <span>더보기</span>
-      </div>
     </div>
-    <template v-if="listVisible">
-      <ul class="announcements-container" key="list">
-        <li
-          v-for="(announcement, idx) in announcements"
-          v-if="idx <= 4"
+    <div>
+      <div class="announcement-header csep" @click="goAnnouncement('')">
+        <span class="facility-name">{{ $t("m.CSEP") }}</span>
+        <span class="facility-description">
+          <span class="plusDiv">
+            <Icon type="android-add" size="13" color="#fff"></Icon>
+            <span>더보기</span>
+          </span>
+        </span>
+      </div>
+      <div class="loading-container" v-if="this.loadingCSEP">
+        <div class="loading-skeleton" v-for="i in Array(2)" :key="i"></div>
+      </div>
+      <div class="notice-sign" v-else-if="this.errorCSPE">
+        <span>{{$t("m.Server_Error")}}</span>
+      </div>
+      <div class="notice-sign" v-else-if="this.csepAnnouncements.length === 0">
+        <span>{{$t("m.No_Announcements")}}</span>
+      </div>
+      <ul class="announcements-container" key="list" v-else>
+        <HomeNoticeItem
+          v-for="(announcement, idx) in csepAnnouncements"
+          v-if="idx < 2"
           :key="announcement.title"
+          :announcement="announcement"
+          :isCSEP="true"
+          :isSW="false"
         >
-          <div class="flex-container">
-            <div class="title">
-              <a class="entry" @click="goAnnouncement(announcement)">
-                {{ announcement.title }}
-              </a>
-              <span class="newAnnotator" v-if="isWithinOneDay(announcement.create_time)">new</span>
-            </div>
-            <div class="date">
-              {{ getOnlyDate(announcement.create_time) }}
-            </div>
-          </div>
-        </li>
+        </HomeNoticeItem>
       </ul>
-      <ul class="announcements-container" key="list">
-
+    </div>
+    <div>
+      <div class="announcement-header sw" @click="goSW">
+        <span class="facility-name">{{ $t("m.SWCenter") }}</span>
+        <span class="facility-description">
+          <span class="plusDiv">
+            <Icon type="android-add" size="13" color="#fff"></Icon>
+            <span>더보기</span>
+          </span>
+        </span>
+      </div>
+      <div class="loading-container" v-if="this.loadingSW">
+        <div class="loading-skeleton" v-for="i in Array(2)" :key="i"></div>
+      </div>
+      <div class="notice-sign" v-else-if="this.errorSW">
+        <span>{{$t("m.Server_Error")}}</span>
+      </div>
+      <div class="notice-sign" v-else-if="this.swAnnouncements.length === 0">
+        <span>{{$t("m.No_Announcements")}}</span>
+      </div>
+      <ul class="announcements-container" key="list" v-else>
+        <HomeNoticeItem
+          v-for="(announcement, idx) in swAnnouncements"
+          v-if="idx < 5"
+          :key="announcement.title"
+          :announcement="announcement"
+          :isCSEP="false"
+          :isSW="true"
+        >
+        </HomeNoticeItem>
       </ul>
-    </template>
+    </div>
   </div>
 </template>
 
 <script>
 import api from '@oj/api'
-import Pagination from '@oj/components/Pagination'
-import NoticeItem from "./NoitceItem.vue";
+import ShineWrapper from "../../../components/ShineWrapper.vue";
+import HomeNoticeItem from "./HomeNoticeItem.vue";
 
 export default {
   name: 'HomeNoticeBox',
-  components: {NoticeItem},
-  data () {
+  components: {HomeNoticeItem, ShineWrapper},
+  data() {
     return {
       limit: 10,
       total: 10,
-      btnLoading: false,
-      announcements: [],
+      loadingCSEP: false,
+      loadingSW: false,
+      errorCSPE: false,
+      errorSW: false,
+      csepAnnouncements: [],
       announcement: '',
-      listVisible: true
+      swAnnouncements: [],
+      swUrl: 'https://swedu.pusan.ac.kr/swedu/index.do',
     }
   },
-  mounted () {
+  mounted() {
     this.getAnnouncementList()
+    this.getSWCenterList()
   },
   methods: {
     getAnnouncementList(page = 1) {
-      this.btnLoading = true;
+      this.loadingCSEP = true;
       api.getAnnouncementList((page - 1) * this.limit, this.limit).then(
         res => {
-          this.btnLoading = false;
-          this.announcements = res.data.data.results;
+          console.log(res)
+          this.loadingCSEP = false;
+          this.csepAnnouncements = res.data.data.results;
           this.total = res.data.data.total;
         },
         () => {
-          this.btnLoading = false;
+          this.loadingCSEP = false;
+          this.errorCSPE = true;
         }
       );
+    },
+    getSWCenterList() {
+      this.loadingSW = true;
+      api.getSWCenterList().then(
+        res => {
+          console.log(res)
+          this.loadingSW = false;
+          this.swAnnouncements = res.data.data.results;
+          this.total = res.data.data.total;
+        },
+        () => {
+          this.loadingSW = false;
+          this.errorSW = true;
+        }
+      );
+    },
+    goSW() {
+      window.open(this.swUrl);
     },
     handleRoute(route) {
       this.$router.push({name: route});
     },
-    getOnlyDate(date) {
-      let onlyDate = new Date(date);
-      return onlyDate.toLocaleDateString();
-    },
-    isWithinOneDay(createTime) {
-      const currentTime = new Date().getTime();
-      const createTimestamp = new Date(createTime).getTime();
-      const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
-
-      return (currentTime - createTimestamp) <= oneDayInMilliseconds;
-    },
     goAnnouncement(announcement) {
-      console.log(announcement)
       this.$router.push({name: "notice", params: {announcement: announcement}});
       // this.announcement = announcement;
       // this.listVisible = false;
-    },
-
+    }
   },
-  computed: {
-
-  }
+  computed: {}
 }
 </script>
 
@@ -104,109 +148,117 @@ export default {
   border: 1px solid #dedede;
   width: 100%;
   height: 457px;
-  padding-left: 30px;
-  padding-right: 30px;
+  padding: 0 30px;
   margin-bottom: 20px;
   transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
   overflow-y: hidden;
 
   .noticeBoxHeader {
-    padding-top: 15px;
-    padding-bottom: 15px;
+    padding: 15px 0;
     display: flex;
     align-items: center;
     justify-content: space-between;
-    border-bottom: 1px solid #dedede;
 
     span:first-child {
       font-weight: 650;
       font-size: 15px;
     }
-
-    .plusDiv{
-      cursor: pointer;
-
-      span {
-        color: #7a7a7a;
-        font-size: 12px;
-      }
-      span:hover{
-        font-weight: bold;
-      }
-    }
   }
 }
 
-.noticeBox:hover{
+.noticeBox:hover {
   border: 1px solid #cccccc;
 }
 
 .announcements-container {
-  margin-top: 10px;
-  margin-bottom: 10px;
-  padding-top: 10px;
-  perspective: 800px;
-  height: 100%;
+  --announcement-date-width: 100px;
+  margin: 10px 0;
   width: 100%;
   display: flex;
   flex-direction: column;
-  li {
-    padding-top: 15px;
-    list-style: none;
-    padding-bottom: 15px;
-    background-color: rgba(251, 251, 251, 0.38);
-    padding-left: 30px;
+}
+
+.announcement-header {
+  height: 40px;
+  width: 100%;
+  border-radius: 7px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 10px;
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  cursor: pointer;
+
+  &.csep {
+    background-color: var(--point-color);
+  }
+
+  &.sw {
+    background-image: linear-gradient(45deg, var(--pnu-blue), var(--pnu-green));
+  }
+
+  .facility-name {
+    font-size: 15px;
+    font-weight: 650;
+    color: white;
+  }
+
+  .facility-description {
+
+
+    span {
+      color: var(--box-background-color);
+      font-size: 12px;
+      font-weight: 900;
+    }
+  }
+
+  &:hover {
+    transform: scale(1.01);
+    text-shadow: 0 0 7px rgba(255, 255, 255, 1);
+
+    font-weight: 900;
+  }
+}
+
+.loading-container {
+  width: 100%;
+  margin: 10px 0;
+
+  .loading-skeleton {
+    width: 100%;
+    height: 35px;
     border-radius: 7px;
-    font-size: 16px;
     margin-bottom: 5px;
+    animation: loading 1s infinite;
 
     &:last-child {
-      border-bottom: none;
-    }
-
-    .flex-container {
-      .title {
-        flex: 1 1;
-        text-align: left;
-        padding-left: 10px;
-
-        a.entry {
-          color: #495060;
-
-          &:hover {
-            color: #4a86c0;
-            //border-bottom: 1px solid #4a86c0;
-          }
-        }
-      }
-
-      .newAnnotator {
-        background-color: #fa6c6c;
-        border-radius: 4px;
-        color: #ffffff;
-        font-size: x-small;
-        text-align: center;
-        width: 23px;
-        margin: auto;
-        padding-left: 3px;
-        padding-right: 3px;
-      }
-
-      .date {
-        flex: none;
-        width: 200px;
-        text-align: center;
-        font-size: small;
-        color: #737373;
-
-
-      }
+      margin-bottom: 0;
     }
   }
 }
 
-li:hover {
-  background-color: rgba(241, 241, 241, 0.45);
+.notice-sign {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 35px;
+  font-size: 15px;
+  font-weight: 650;
+  color: var(--ps-content-text-color);
+  margin: 10px 0;
+}
+
+@keyframes loading {
+  0% {
+    background-color: #e3e3e3;
+  }
+  50% {
+    background-color: #f5f5f5;
+  }
+  100% {
+    background-color: #e3e3e3;
+  }
 }
 </style>
 
