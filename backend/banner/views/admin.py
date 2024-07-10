44f152279bfa05729f28260a6a4219a1948afc2b
hunsy9 <juniper0917@gmail.com>
2024-07-10 21:49:35 +0900

    feat: 배너 수정 API 추가(POST)

--- backend/banner/views/admin.py
+++ backend/banner/views/admin.py
@@ -2,15 +2,18 @@ import os
 
 from django.core.exceptions import ValidationError
 from django.core.validators import URLValidator
+from django.db import transaction
 
 from account.decorators import super_admin_required
 from banner.models import Banner
 from banner.serializers import ImageUploadForm, BannerAdminSerializer
 from oj import settings
 from utils.api import APIView
+from utils.constants import BANNER_VISIBLE_LIMIT
 from utils.shortcuts import rand_str
 
 import logging
+
 logger = logging.getLogger(__name__)
 
 
@@ -86,31 +89,78 @@ class AdminBannerAPIView(APIView):
         return self.success(BannerAdminSerializer(new_banner).data)
 
     @super_admin_required
-    def put(self, request):
-        """
-        어드민 페이지-홈 배너 관리 페이지에서 등록된 배너를 수정합니다.
-        다음과 같은 데이터가 필요할 수 있습니다.
-        - 신규 배너 이미지
-        - 신규 URL
-        """
-        pass
-
-    @super_admin_required
     def delete(self, request):
         """
         어드민 페이지-홈 배너 관리 페이지에서 등록된 배너를 삭제합니다.
         """
-        # target_banner = Banner.objects.get(id=request.data.get('id'))
-        #
-        # if not target_banner.order:
-        #     target_banner.delete()
-        pass
+        try:
+            target_banner = Banner.objects.get(id=request.data.get('id'))
+        except Banner.DoesNotExist:
+            return self.error("Invalid Banner")
+        Banner.remove(target_banner)
+        return self.success("delete success")
 
 
+class EditAdminBannerAPIView(APIView):
 
+    @super_admin_required
+    def post(self, request):
+        """
+        어드민 페이지-홈 배너 관리 페이지에서 등록된 배너를 수정합니다.
+        다음과 같은 수정 기능을 포함합니다.
+        - 배너 이미지, 연결 링크 수정
+        """
+        data = request.data
+        form = ImageUploadForm(request.POST, request.FILES)
 
+        # 수정 타겟
+        try:
+            target_banner = Banner.objects.get(id=request.GET.get('id'))
+        except Banner.DoesNotExist:
+            return self.error("Invalid Banner")
 
+        # 연결 링크 변경
+        if data["link_url"]:
+            url_validator = URLValidator()
+            # URL 유효성 검사
+            try:
+                url_validator(data["link_url"])
+            except ValidationError:
+                return self.error("Invalid URL")
+            target_banner.link_url = data["link_url"]
 
+        # 이미지 변경 사항이 있는 경우
+        if form.is_valid():
+            banner_image = form.cleaned_data["image"]
+            # 파일 크기 검사
+            if banner_image.size > 2 * 1024 * 1024:
+                return self.error("Picture is too large")
+
+            # 파일 확장자 검사
+            suffix = os.path.splitext(banner_image.name)[-1].lower()
+            if suffix not in [".gif", ".jpg", ".jpeg", ".bmp", ".png"]:
+                return self.error("Unsupported file format")
+
+            # 기존 이미지 삭제
+            original_image = target_banner.banner_image
+            if os.path.isfile(os.path.join(settings.BANNER_DIR, original_image)):
+                os.remove(os.path.join(settings.BANNER_DIR, original_image))
+
+            # 배너 이미지 주소를 랜덤으로 생성
+            name = rand_str(10) + suffix
+
+            # 배너 이미지 저장
+            with open(os.path.join(settings.BANNER_DIR, name), "wb") as img:
+                for chunk in banner_image:
+                    img.write(chunk)
+
+            # 신규 경로 지정
+            target_banner.banner_image = f"{settings.BANNER_URI_PREFIX}/{name}"
+        else:
+            self.error("Invalid file content")
+
+        target_banner.save()
+        return self.success(BannerAdminSerializer(target_banner).data)
 
     @super_admin_required
     def put(self, request):
