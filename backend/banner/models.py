14fe5ba52a4c84839f06e24e2d631d0071dfe22c
hunsy9 <juniper0917@gmail.com>
2024-07-10 16:09:40 +0900

    feat: Banner 모델 추가

--- backend/banner/models.py
+++ backend/banner/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Banner(models.Model):
+    banner_image = models.TextField()
+    link_url = models.URLField()
+    visible = models.BooleanField(default=False)
+    order = models.PositiveIntegerField(null=True, blank=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    last_update_time = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        db_table = "banner"
+        ordering = ("order","-create_time")
