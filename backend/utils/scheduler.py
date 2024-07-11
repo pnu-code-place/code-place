35cdfd4569083555b73ea15e65750b69e97c379f
hunsy9 <juniper0917@gmail.com>
2024-07-12 02:11:41 +0900

    fix: main django에서 임시적으로 스케쥴러 중지

--- backend/utils/scheduler.py
+++ backend/utils/scheduler.py
@@ -14,11 +14,12 @@ class Scheduler:
         self.add_jobs()
 
     def add_jobs(self):
-        self.scheduler.add_job(UserScore.calculate_basis, 'cron', hour=0)
-        self.scheduler.add_job(UserScore.calculate_fluctuation, 'interval', minutes=1)
-        self.scheduler.add_job(call_update_weekly_stats, 'cron', day_of_week='mon', hour=0, minute=0)
-        self.scheduler.add_job(call_update_bonus_problem, 'cron', day_of_week='mon', hour=0, minute=0)
-        self.scheduler.add_job(scrap_and_update, 'interval', hours=2, next_run_time=timezone.now()+timedelta(seconds=30))
+        pass
+        # self.scheduler.add_job(UserScore.calculate_basis, 'cron', hour=0)
+        # self.scheduler.add_job(UserScore.calculate_fluctuation, 'interval', minutes=1)
+        # self.scheduler.add_job(call_update_weekly_stats, 'cron', day_of_week='mon', hour=0, minute=0)
+        # self.scheduler.add_job(call_update_bonus_problem, 'cron', day_of_week='mon', hour=0, minute=0)
+        # self.scheduler.add_job(scrap_and_update, 'interval', hours=2, next_run_time=timezone.now()+timedelta(seconds=30))
 
     def start(self):
         self.scheduler.start()
