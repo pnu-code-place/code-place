119ffec96c52e9515ca874c6c75333c54c7edb73
hunsy9 <juniper0917@gmail.com>
2024-07-12 02:08:05 +0900

    chore: 개발/운영 환경 분리

---
+++ scheduler/csep_scheduler/dev_settings.py
@@ -0,0 +1,20 @@
+import os
+
+from utils.shortcuts import get_env
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': get_env('POSTGRES_HOST', '127.0.0.1'),
+        'PORT': get_env('POSTGRES_PORT', '5435'),
+        'NAME': get_env('POSTGRES_DB', 'onlinejudge'),
+        'USER': get_env('POSTGRES_USER', 'onlinejudge'),
+        'PASSWORD': get_env('POSTGRES_PASSWORD', 'onlinejudge')
+    }
+}
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
