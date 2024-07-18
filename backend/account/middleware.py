731ea706b4df4d32609ef8e8c0959748808ddc10
hunsy9 <juniper0917@gmail.com>
2024-07-19 02:39:21 +0900

    bugfix: 초기 로그인 시 세션 키가 DB에 들어가지 않는 오류 signal로 해결

--- backend/account/middleware.py
+++ backend/account/middleware.py
@@ -1,5 +1,7 @@
 from django.conf import settings
+from django.contrib.auth import user_logged_in
 from django.db import connection
+from django.dispatch import receiver
 from django.utils.timezone import now
 from django.utils.deprecation import MiddlewareMixin
 
@@ -22,15 +24,20 @@ class APITokenAuthMiddleware(MiddlewareMixin):
 class SessionRecordMiddleware(MiddlewareMixin):
     def process_request(self, request):
         request.ip = request.META.get(settings.IP_HEADER, request.META.get("REMOTE_ADDR"))
-        if request.user.is_authenticated:
+        if request.user.is_authenticated and request.session.session_key:
             session = request.session
-            session["user_agent"] = request.META.get("HTTP_USER_AGENT", "")
-            session["ip"] = request.ip
+            if 'user_agent' not in session or 'ip' not in session:
+                session["user_agent"] = request.META.get("HTTP_USER_AGENT", "")
+                session["ip"] = request.ip
+                session.modified = True
             session["last_activity"] = now()
-            user_sessions = request.user.session_keys
-            if session.session_key not in user_sessions:
-                user_sessions.append(session.session_key)
-                request.user.save()
+            session.save()
+
+@receiver(user_logged_in)
+def add_session_key(sender, request, user, **kwargs):
+    if request.session.session_key and request.session.session_key not in user.session_keys:
+        user.session_keys.append(request.session.session_key)
+        user.save()
 
 
 class AdminRoleRequiredMiddleware(MiddlewareMixin):
