c45e8965ae16dff793c387f1e5e5f14f0e6bab3c
유승훈 <101303791+hunsy9@users.noreply.github.com>
2024-07-18 22:26:48 +0900

    [BE] 링크 공지사항 도메인 제거 (#125)
    * chore: 링크 공지사항 모델 삭제
    
    * chore: 링크 공지사항 관련 상수 삭제
    
    * chore: 링크 공지사항 모델 삭제
    
    * chore: 링크 공지사항 GET API 삭제

--- backend/announcement/models.py
+++ backend/announcement/models.py
@@ -16,15 +16,3 @@ class Announcement(models.Model):
     class Meta:
         db_table = "announcement"
         ordering = ("-create_time",)
-
-
-class LinkAnnouncement(models.Model):
-    la_id = models.IntegerField(primary_key=True)
-    title = models.TextField()
-    url = models.TextField()
-    create_time = models.TextField()
-    new_flag = models.BooleanField(default=False)
-
-    class Meta:
-        db_table = "link_announcement"
-        ordering = ("-la_id",)
--- backend/utils/constants.py
+++ backend/utils/constants.py
@@ -37,9 +37,6 @@ class Difficulty(Choices):
 
 CONTEST_PASSWORD_SESSION_KEY = "contest_password"
 
-LINK_NOTICE_SCRAPING_URL = "https://swedu.pusan.ac.kr/swedu/31630/subview.do"
-LINK_NOTICE_LIMIT = 5
-
 BANNER_VISIBLE_LIMIT = 5
 
 class ProblemField:
