755ccdf75b876c17476e7be0f57fcd98a73da565
hunsy9 <juniper0917@gmail.com>
2024-07-18 22:26:02 +0900

    chore: 링크 공지사항 GET API 삭제

--- backend/announcement/urls/oj.py
+++ backend/announcement/urls/oj.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url
 
-from ..views.oj import AnnouncementAPI, SWAnnouncementAPI
+from ..views.oj import AnnouncementAPI
 
 urlpatterns = [
     url(r"^announcement/?$", AnnouncementAPI.as_view(), name="announcement_api"),
-    url(r"^sw_center_notice/?$", SWAnnouncementAPI.as_view(), name="sw_center_notice_api"),
 ]
