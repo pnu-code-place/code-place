b9e3432d03fc04c633690b911304c948b67126e8
유승훈 <101303791+hunsy9@users.noreply.github.com>
2024-07-20 23:13:21 +0900

    #134 [BE] RSS 공지사항 파싱 및 GET API 구현 (#135)
    * chore: RSS 피드 URL 정의
    
    * feat: API 구현 및 엔드포인트 정의
    
    * feat: RSS JSON 반환 Serializer 선언
    
    * test: 테스트 코드 작성

--- backend/contents/serializers.py
+++ backend/contents/serializers.py
@@ -4,3 +4,7 @@ class HomeStatistics(serializers.Serializer):
     accepted_problem_length = serializers.IntegerField()
     ended_contest_length = serializers.IntegerField()
 
+class RSSItemSerializer(serializers.Serializer):
+    title = serializers.CharField()
+    link = serializers.URLField()
+    pubDate = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
--- backend/contents/urls/oj.py
+++ backend/contents/urls/oj.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from contents.views.oj import GetHomeStatisticsAPI
+from contents.views.oj import GetHomeStatisticsAPI, GetHomeRSSNoticeAPI
 
 urlpatterns = [
     url(r"^home_statistics/?$", GetHomeStatisticsAPI.as_view(), name="home_statistics"),
+    url(r"^sw_center_notice/?$", GetHomeRSSNoticeAPI.as_view(), name="home_rss_notice"),
 ]
