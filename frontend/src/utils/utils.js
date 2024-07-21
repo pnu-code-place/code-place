8a55385a6a945290f78ebdc83ded8529efd3e8e4
minmunui <ehdwls1638@gmail.com>
2024-07-22 01:28:10 +0900

    #118 : feat : 비밀번호 변경 및 아바타변경 추가

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -127,6 +127,41 @@ function getLanguages() {
   })
 }
 
+function xmlToJson(xml) {
+  let obj = {};
+
+  if (xml.nodeType === 1) { // Element node
+    if (xml.attributes.length > 0) {
+      obj["@attributes"] = {};
+      for (let j = 0; j < xml.attributes.length; j++) {
+        const attribute = xml.attributes.item(j);
+        obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+      }
+    }
+  } else if (xml.nodeType === 3) { // Text node
+    obj = xml.nodeValue;
+  }
+
+  // 자식 노드를 순회
+  if (xml.hasChildNodes()) {
+    for(let i = 0; i < xml.childNodes.length; i++) {
+      const item = xml.childNodes.item(i);
+      const nodeName = item.nodeName;
+      if (typeof(obj[nodeName]) === "undefined") {
+        obj[nodeName] = xmlToJson(item);
+      } else {
+        if (typeof(obj[nodeName].push) === "undefined") {
+          const old = obj[nodeName];
+          obj[nodeName] = [];
+          obj[nodeName].push(old);
+        }
+        obj[nodeName].push(xmlToJson(item));
+      }
+    }
+  }
+  return obj;
+}
+
 export default {
   submissionMemoryFormat: submissionMemoryFormat,
   submissionTimeFormat: submissionTimeFormat,
