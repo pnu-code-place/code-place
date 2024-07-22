3ce63889542f0a5bb3f2bda5dfcb95f9ff48ef53
llddang <bodyness@naver.com>
2024-07-22 22:07:48 +0900

    feat: 홈 배너 관리 페이지 제목 구현

--- frontend/src/pages/admin/views/general/HomeBannerManagement.vue
+++ frontend/src/pages/admin/views/general/HomeBannerManagement.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Home Banner Management</div>
+  <div style="padding: 30px;">
+    <section style="display: flex; justify-content: space-between;">
+      <h1 class="title">{{$t('m.Home_Banner_Management')}}</h1>
+      <span class="alert-description" style="display: flex; gap: 10px; align-items: center;">
+        <img src="@/assets/information_icon.svg" width="17px" />
+        배너 활성화는 최대 5개까지 가능합니다.
+      </span>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -9,4 +17,15 @@
 </script>
 
 <style lang="less" scoped>
+.title {
+  margin: 0;
+  color: #333;
+  font-size: 18px;
+  font-weight: 300;
+}
+.alert-description {
+  color: #666;
+  font-size: 18px;
+  font-weight: 500;
+}
 </style>
