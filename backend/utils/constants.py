9f53ae1d37faa6d4ea97f8705f7a7951e4df9b38
hunsy9 <juniper0917@gmail.com>
2024-07-22 18:05:56 +0900

    chore: 서비스 가입 이메일 suffix 정의

--- backend/utils/constants.py
+++ backend/utils/constants.py
@@ -43,6 +43,9 @@ RSS_FEED_URL = "https://swedu.pusan.ac.kr/bbs/swedu/6906/rssList.do?row=50"
 
 GENERATE_MOCK_USERNAME_URL = "https://www.rivestsoft.com/nickname/getRandomNickname.ajax"
 
+EMAIL_SUFFIX="@pusan.ac.kr"
+
+
 class ProblemField:
     fields = ['implementation', 'math', 'datastructure', 'search', 'sorting']
     strToInt = {
