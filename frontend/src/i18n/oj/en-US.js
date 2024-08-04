806d93ae0b1376bec437f2b7ee9c9ea969de98c5
llddang <77055208+llddang@users.noreply.github.com>
2024-08-05 04:06:21 +0900

    #139 [FE] 배너 관리 페이지 구현 (#164)
    * feat: 홈 배너 관리 페이지 router 등록
    
    * feat: 사이드바에 홈 배너 관리 페이지로 이동할 수 있는 버튼 만들기
    
    * feat: 홈 배너 관리 페이지 제목 구현
    
    * feat: 홈 배너 관리 페이지에서 필요한 단어 추가
    
    * design: 홈 배너 관리 페이지에서 배너 표 구현
    
    * design: 테이블 font-weight 수정
    
    * design: 추가, 순서 저장 버튼 구현
    
    * feat: 필요한 이벤트 함수 정의
    
    * design: 배너가 존재하지 않을 때의 문구 구현
    
    * feat: drag 애니메이션 추가 및 순서 저장 버튼 삭제
    
    * feat: Confirm 모달 생성
    
    * feat: confirm 모달 수정
    
    * feat: Image drag and drop box 구현
    
    * feat: ImageDragAndDropBox 에 사용되는 한글 정의
    
    * feat: ConfirmModal의 props 받는 방법 수정
    
    * design: 모달 위치 수정
    
    * feat: 이미지 최대 사이즈 수정
    
    * design: 홈 배너 추가 모달 구현
    
    * design: 배너 수정 모달 구현
    
    * refact: 변수명 통일
    
    * feat: 모달 연결
    
    * feat: 홈 배너 수정 관련 한글 정의
    
    * feat: 배너 관리 관련 api 정의
    
    * api: 배너 조회 api 연결
    
    * feat: 배너 추가 api 연결
    
    * feat: api 순서 변경
    
    * api: 배너 조회 & 배너 삭제 api 연결
    
    * feat: 배너 추가가 완료되었을 때 모달이 닫히도록 수정
    
    * feat: 배너의 수정이 완료되었을 때 모달이 닫히도록 수정
    
    * feat: 모달 활성화 api 연결
    
    * feat: 배너 추가하는 모달에서 인풋값 검증
    
    * feat: 배너 수정 모달에서 인풋값에 대한 검증 로직 추가하기
    
    * feat: 배너 순서 조정 api 연결

--- frontend/src/i18n/oj/en-US.js
+++ frontend/src/i18n/oj/en-US.js
@@ -439,8 +439,9 @@ export const m = {
   NotAchieved: "미달성",
 
   // ProfileSetting.vue
-  Nickname : "닉네임",
-  Nickname_Description: "다른 사람들에게 보여지며 마이페이지 주소로 사용됩니다.",
+  Nickname: "닉네임",
+  Nickname_Description:
+    "다른 사람들에게 보여지며 마이페이지 주소로 사용됩니다.",
   MajorAndDeparture: "단과대학/학과",
   FavoriteLanguage: "주로 사용하는 언어",
   CheckDuplicate: "중복확인",
@@ -452,7 +453,8 @@ export const m = {
   School: "단과대학",
   Mood: "소개",
   Mood_Description: "256자까지 입력이 가능하며, 마이페이지에 노출됩니다.",
-  Github_Description: "Github 아이디를 입력해주세요. 마이페이지에 링크와 함께 표시됩니다.",
+  Github_Description:
+    "Github 아이디를 입력해주세요. 마이페이지에 링크와 함께 표시됩니다.",
   Nickname_Duplicated: "이미 사용중인 닉네임입니다.",
   Nickname_Required: "닉네임을 입력해주세요.",
   College_Required: "단과대학을 먼저 선택해주세요.",
@@ -477,8 +479,9 @@ export const m = {
   Button_Enter: "확인",
 
   // footer.vue
-  PNUCSEP : "부산대학교 코딩역량강화플랫폼",
-  AddressPNUCSEP : "부산광역시 금정구 부산대학로63번길 2 (장전동) 부산대학교 소프트웨어융합교육원",
+  PNUCSEP: "부산대학교 코딩역량강화플랫폼",
+  AddressPNUCSEP:
+    "부산광역시 금정구 부산대학로63번길 2 (장전동) 부산대학교 소프트웨어융합교육원",
   UserGuide: "사용자 가이드",
 
   Unknown_Error: "알 수 없는 오류가 발생했습니다.",
@@ -490,7 +493,14 @@ export const m = {
   Please_Input_Current_Password: "현재 비밀번호를 입력해 주세요.",
   Please_Input_New_Password: "새 비밀번호를 입력해 주세요.",
   Please_Input_New_Password_Again: "새 비밀번호를 다시 입력해 주세요.",
-  New_Password_Cannot_Be_The_Same_As_The_Current_Password: "새 비밀번호는 현재 비밀번호와 같을 수 없습니다.",
-  Invalid_Password_Format: "비밀번호는 8자 이상, 영문, 숫자, 특수문자를 포함해야 합니다.",
+  New_Password_Cannot_Be_The_Same_As_The_Current_Password:
+    "새 비밀번호는 현재 비밀번호와 같을 수 없습니다.",
+  Invalid_Password_Format:
+    "비밀번호는 8자 이상, 영문, 숫자, 특수문자를 포함해야 합니다.",
   You_Will_Be_Logged_Out_In_5_Seconds: "5초 후 로그아웃됩니다...",
+
+  // ImageDragAndDrop.vue
+  Image_Max_Size_Notice: "최대 10MB 이하 jpeg, png 첨부 가능",
+  Image_Max_Size_Alert: "10MB 이하의 사진을 올려주세요.",
+  Bring_Image: "이미지 가져오기",
 };
---
+++ frontend/src/pages/admin/views/general/HomeBannerManagement.vue
@@ -0,0 +1,237 @@
+<template>
+  <div style="padding: 20px; min-width: 1200px">
+    <section
+      style="display: flex; justify-content: space-between; margin: 12px"
+    >
+      <h1 class="title">{{ $t("m.Home_Banner_Management") }}</h1>
+      <span
+        class="alert-description"
+        style="display: flex; gap: 10px; align-items: center"
+      >
+        <img src="@/assets/information_icon.svg" width="17px" />
+        {{ $t("m.Home_Banner_Notice") }}
+      </span>
+    </section>
+    <hr style="border: 0.5px solid #e6e6e6" />
+    <section style="padding: 20px">
+      <div
+        style="
+          display: grid;
+          padding: 12px;
+          border-bottom: 1px solid #e6e6e6;
+          justify-items: center;
+          grid-template-columns: 0.2fr 0.4fr 2.5fr repeat(3, 1fr) 0.6fr;
+        "
+      >
+        <p></p>
+        <p class="table-title">{{ $t("m.Home_Banner_ID") }}</p>
+        <p class="table-title">{{ $t("m.Banner_Image") }}</p>
+        <p class="table-title">{{ $t("m.Create_Time") }}</p>
+        <p class="table-title">{{ $t("m.Last_Update_Time") }}</p>
+        <p class="table-title">{{ $t("m.Enable_Disable") }}</p>
+        <p class="table-title">{{ $t("m.Setting") }}</p>
+      </div>
+      <div>
+        <div
+          v-if="banners.length === 0"
+          class="table-title"
+          style="width: 100%; text-align: center; margin: 50px 0px"
+        >
+          {{ $t("m.Home_Banner_Does_Not_Exist") }}
+        </div>
+        <draggable
+          v-else
+          tag="ul"
+          :list="banners"
+          handle=".handle"
+          v-bind="dragOptions"
+          @end="handleBannerOrderChange"
+        >
+          <li
+            v-for="(banner, idx) in banners"
+            :key="banner.id"
+            class="drag-item"
+          >
+            <i class="el-icon-fa-bars handle" style="color: #868991"></i>
+            <span class="drag-item-text" style="font-size: 18px">
+              {{ banner.id }}
+            </span>
+            <img :src="banner.banner_image" width="100%" />
+            <span class="drag-item-text">
+              {{ banner.create_time | localtime }}
+            </span>
+            <span class="drag-item-text">
+              {{ banner.last_update_time | localtime }}
+            </span>
+            <el-switch
+              v-model="banner.visible"
+              @change="handleVisibleSwitchChange(banner.id, banner.visible)"
+            >
+            </el-switch>
+            <div>
+              <icon-btn
+                :name="$t('m.Icon_Edit')"
+                icon="edit"
+                @click.native="handleModifyButtonClick(banner)"
+              ></icon-btn>
+              <icon-btn
+                :name="$t('m.Icon_Delete')"
+                icon="trash"
+                @click.native="handleDeleteButtonClick(banner.id)"
+              ></icon-btn>
+            </div>
+          </li>
+        </draggable>
+      </div>
+    </section>
+    <section style="margin: 12px">
+      <button
+        class="button"
+        style="color: white; background-color: #409eff"
+        @click="handleAddButtonClick"
+      >
+        <i class="el-icon-plus"></i> {{ $t("m.Add") }}
+      </button>
+    </section>
+    <AddBannerModal v-if="addModalOpen" @onClose="handleModalClose" />
+    <ModifyBannerModal
+      v-if="modifyModalOpen"
+      @onClose="handleModalClose"
+      :banner="this.currentBanner"
+    />
+  </div>
+</template>
+
+<script>
+import api from "../../api.js";
+import draggable from "vuedraggable";
+import AddBannerModal from "./HomeBanner/AddBannerModal";
+import ModifyBannerModal from "./HomeBanner/ModifyBannerModal";
+
+export default {
+  name: "home-banner-management",
+  components: {
+    draggable,
+    AddBannerModal,
+    ModifyBannerModal,
+  },
+  data() {
+    return {
+      addModalOpen: false,
+      modifyModalOpen: false,
+      banners: [],
+    };
+  },
+  beforeRouteEnter(to, from, next) {
+    next((vm) => {
+      api.getBanners().then((res) => {
+        vm.banners = res.data.data;
+      });
+    });
+  },
+  methods: {
+    getBanners() {
+      api.getBanners().then((res) => {
+        this.banners = res.data.data;
+      });
+    },
+    handleVisibleSwitchChange(id, visible) {
+      api.editEnableBanner(id, { visible: visible }).then((res) => {
+        if (res.status === 200) this.getBanners();
+      });
+    },
+    handleBannerOrderChange(id) {
+      const reorder_list = this.banners.map((banner) => banner.id);
+
+      api.reorderBanner({ reorder_list: reorder_list }).catch((err) => {
+        this.banners = err.data.data;
+      });
+    },
+    handleAddButtonClick() {
+      this.addModalOpen = true;
+    },
+    handleModifyButtonClick(banner) {
+      this.currentBanner = banner;
+      this.modifyModalOpen = true;
+    },
+    handleDeleteButtonClick(id) {
+      api.deleteBanner(id).then((res) => {
+        if (res.status === 200) this.getBanners();
+      });
+    },
+    handleModalClose() {
+      this.getBanners();
+      this.addModalOpen = false;
+      this.modifyModalOpen = false;
+    },
+  },
+  computed: {
+    dragOptions() {
+      return {
+        ghostClass: "ghost",
+      };
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.title {
+  margin: 0;
+  color: #333;
+  font-size: 18px;
+  font-weight: 300;
+}
+.alert-description {
+  color: #666;
+  font-size: 18px;
+  font-weight: 500;
+}
+
+.table-title {
+  color: #666;
+  font-size: 18px;
+  font-weight: 700;
+}
+
+ul,
+li,
+p {
+  margin: 0;
+  padding: 0;
+}
+
+.drag-item {
+  display: grid;
+  grid-template-columns: 0.2fr 0.4fr 2.5fr repeat(3, 1fr) 0.6fr;
+  list-style-type: none;
+  list-style: none;
+  padding: 15px;
+  justify-items: center;
+  align-items: center;
+  border-bottom: 1px solid #e6e6e6;
+}
+.drag-item-text {
+  color: #606266;
+  font-size: 15px;
+  font-weight: 600;
+}
+
+.button {
+  padding: 8px 16px;
+  border: none;
+  border-radius: 5px;
+  background-color: white;
+  font-size: 13px;
+  font-weight: 450;
+  cursor: pointer;
+  &:hover {
+    opacity: 70%;
+  }
+}
+
+.ghost {
+  opacity: 0.1;
+  background: #409eff;
+}
+</style>
