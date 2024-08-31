154f3b2102d52e59038b655732ba9468b90a7ca3
hunsy9 <juniper0917@gmail.com>
2024-08-31 23:09:38 +0900

    fix: ContestSubmissionListAPI 어드민 API로 이동

--- backend/submission/urls/admin.py
+++ backend/submission/urls/admin.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
 from ..views.admin import SubmissionRejudgeAPI
+from ..views.oj import ContestSubmissionListAPI
 
 urlpatterns = [
     url(r"^submission/rejudge?$", SubmissionRejudgeAPI.as_view(), name="submission_rejudge_api"),
+    url(r"^contest_submissions/?$", ContestSubmissionListAPI.as_view(), name="contest_submission_list_api"),
 ]
