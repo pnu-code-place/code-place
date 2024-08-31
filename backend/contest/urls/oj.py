6b60688961fa14ba9eda6a879805d8c1dcc33700
유승훈 <101303791+hunsy9@users.noreply.github.com>
2024-08-31 23:11:36 +0900

    [BE] 컨테스트 페이지 기능 어드민 페이지로 이동 (#209)
    * fix: 채점현황 사진이 누락된 오류 해결
    
    * fix: ContestSubmissionListAPI 어드민 API로 이동
    
    * fix: ContestRankAPI 어드민 API로 이동

--- backend/contest/urls/oj.py
+++ backend/contest/urls/oj.py
@@ -4,7 +4,6 @@ from ..views.oj import ContestAnnouncementListAPI, ContestHistoryListAPI, Contes
     ContestNotStartedListAPI
 from ..views.oj import ContestPasswordVerifyAPI, ContestAccessAPI
 from ..views.oj import ContestListAPI, ContestAPI
-from ..views.oj import ContestRankAPI
 
 urlpatterns = [
     url(r"^contests/?$", ContestListAPI.as_view(), name="contest_list_api"),
@@ -15,5 +14,4 @@ urlpatterns = [
     url(r"^contest/password/?$", ContestPasswordVerifyAPI.as_view(), name="contest_password_api"),
     url(r"^contest/announcement/?$", ContestAnnouncementListAPI.as_view(), name="contest_announcement_api"),
     url(r"^contest/access/?$", ContestAccessAPI.as_view(), name="contest_access_api"),
-    url(r"^contest_rank/?$", ContestRankAPI.as_view(), name="contest_rank_api"),
 ]
