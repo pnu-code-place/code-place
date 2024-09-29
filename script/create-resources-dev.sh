651bf7fe9733b12e339101d878ed18998ca7d580
hunsy9 <juniper0917@gmail.com>
2024-09-29 15:29:45 +0900

    feat: 운영/개발 환경 메인 볼륨 생성 스크립트 작성

--- script/create-resources-dev.sh
+++ script/create-resources-dev.sh
@@ -1,7 +1,6 @@
-# 데이터 볼륨 생성
 sudo docker volume create \
   --driver local \
   --opt type=none \
   --opt o=bind \
   --opt device=$(dirname $(pwd))/backend/data/backend \
-  backend-data-prod
+  backend-data-dev
