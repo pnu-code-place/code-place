65ac877036a8796da6e80d31948f9913ad21f485
hunsy9 <juniper0917@gmail.com>
2024-11-15 17:15:32 +0900

    feat: 클라이언트 조회 API 추가

---
+++ backend/popup/views/oj.py
@@ -0,0 +1,9 @@
+from popup.models import Popup
+from popup.serializers import PopupSerializer
+from utils.api import APIView
+
+
+class PopupAPIView(APIView):
+    def get(self, request):
+        popups = Popup.objects.filter(visible=True).order_by('order')
+        return self.success(PopupSerializer(popups, many=True).data)
