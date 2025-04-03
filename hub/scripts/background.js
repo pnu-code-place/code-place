c5a500974b0c33ad5e625375264c6360bac2b270
Park Junwoo <boksam1017@gmail.com>
2025-04-03 09:45:01 +0900

    URL detect by background worker & send message to content-script

--- hub/scripts/background.js
+++ hub/scripts/background.js
@@ -1,17 +1,21 @@
+// TODO: This PROBLEM_URL_REGEX is defined in code_place/variables.js
+// We should figure it out how to import the variables from there
+const PROBLEM_URL_REGEX = /localhost:8080\/problem\/\d+/;
+
 function handleMessage(request, sender, sendResponse) {
   if (request && request.closeWebPage === true && request.isSuccess === true) {
     /* Set username */
     chrome.storage.local.set(
       { CodePlaceHub_username: request.username } /* , () => {
       window.localStorage.CodePlaceHub_username = request.username;
-    } */,
+    } */
     );
 
     /* Set token */
     chrome.storage.local.set(
       { CodePlaceHub_token: request.token } /* , () => {
       window.localStorage[request.KEY] = request.token;
-    } */,
+    } */
     );
 
     /* Close pipe */
@@ -40,3 +44,27 @@ function handleMessage(request, sender, sendResponse) {
 }
 
 chrome.runtime.onMessage.addListener(handleMessage);
+
+/**
+ * Check URL and IF CURRENT URL IS PROBLEM PAGE,
+ * Send message to scripts/code_place/code_place.js
+ */
+const handleProblemPageDetection = (tabId, url) => {
+  if (PROBLEM_URL_REGEX.test(url)) {
+    chrome.tabs.sendMessage(tabId, {
+      action: "url_changed_to_problem_detail_page",
+      url: url,
+    });
+    return true;
+  }
+  return false;
+};
+
+/**
+ * Listen to tab update to cover the case if the user directly enters to the problem page
+ */
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete" && tab.url) {
+    handleProblemPageDetection(tabId, tab.url);
+  }
+});
