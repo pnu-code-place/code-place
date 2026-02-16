e7d76f98c3014c0347a1335297d70e65aeed5ff9
Minchan Kim <kimmc3423@naver.com>
2026-02-16 16:57:03 +0900

    eslint-loader 삭제 및 formatter적용

--- frontend/build/webpack.base.conf.js
+++ frontend/build/webpack.base.conf.js
@@ -1,24 +1,24 @@
-'use strict'
-const path = require('path')
-const glob = require('glob')
-const webpack = require('webpack')
-const utils = require('./utils')
-const config = require('../config')
-const vueLoaderConfig = require('./vue-loader.conf')
-const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin')
+"use strict"
+const path = require("path")
+const glob = require("glob")
+const webpack = require("webpack")
+const utils = require("./utils")
+const config = require("../config")
+const vueLoaderConfig = require("./vue-loader.conf")
+const HtmlWebpackIncludeAssetsPlugin = require("html-webpack-include-assets-plugin")
 
-function resolve (dir) {
-  return path.join(__dirname, '..', dir)
+function resolve(dir) {
+  return path.join(__dirname, "..", dir)
 }
 
-function getEntries () {
+function getEntries() {
   const base = {
-    'oj': ['./src/pages/oj/index.js'],
-    'admin': ['./src/pages/admin/index.js']
+    oj: ["./src/pages/oj/index.js"],
+    admin: ["./src/pages/admin/index.js"],
   }
-  if (process.env.USE_SENTRY === '1') {
-    Object.keys(base).forEach(entry => {
-      base[entry].push('./src/utils/sentry.js')
+  if (process.env.USE_SENTRY === "1") {
+    Object.keys(base).forEach((entry) => {
+      base[entry].push("./src/utils/sentry.js")
     })
   }
   return base
@@ -27,99 +27,97 @@ function getEntries () {
 // get all entries
 const entries = getEntries()
 console.log("All entries: ")
-Object.keys(entries).forEach(entry => {
+Object.keys(entries).forEach((entry) => {
   console.log(entry)
-  entries[entry].forEach(ele => {
+  entries[entry].forEach((ele) => {
     console.log("- %s", ele)
   })
   console.log()
 })
 
 // prepare vendor asserts
-const globOptions = {cwd: resolve('static/js')};
-let vendorAssets = glob.sync('vendor.dll.*.js', globOptions);
-vendorAssets = vendorAssets.map(file => 'static/js/' + file)
-
+const globOptions = { cwd: resolve("static/js") }
+let vendorAssets = glob.sync("vendor.dll.*.js", globOptions)
+vendorAssets = vendorAssets.map((file) => "static/js/" + file)
 
 module.exports = {
   entry: entries,
   output: {
     path: config.build.assetsRoot,
-    filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+    filename: "[name].js",
+    publicPath:
+      process.env.NODE_ENV === "production"
+        ? config.build.assetsPublicPath
+        : config.dev.assetsPublicPath,
   },
   resolve: {
-    modules: ['node_modules'],
-    extensions: ['.js', '.vue', '.json'],
+    modules: ["node_modules"],
+    extensions: [".js", ".vue", ".json"],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
-      '@oj': resolve('src/pages/oj'),
-      '@admin': resolve('src/pages/admin'),
-      '~': resolve('src/components')
-    }
+      vue$: "vue/dist/vue.esm.js",
+      "@": resolve("src"),
+      "@oj": resolve("src/pages/oj"),
+      "@admin": resolve("src/pages/admin"),
+      "~": resolve("src/components"),
+    },
   },
   module: {
     rules: [
       {
-        test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        enforce: 'pre',
-        include: [resolve('src')],
-        options: {
-          formatter: require('eslint-friendly-formatter')
-        }
-      },
-      {
         test: /\.vue$/,
-        loader: 'vue-loader',
-        options: vueLoaderConfig
+        loader: "vue-loader",
+        options: vueLoaderConfig,
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader?cacheDirectory=true',
+        loader: "babel-loader?cacheDirectory=true",
         // include -> inclue 안에 있는 모듈 '만' babel이 로드 합니다.
         // exclude -> exclude 안에 있는 모듈 '만' babel 로드 시 제외 합니다.
         // 따라서 현재 include 안에 node_modules 가 없으므로, node_modules 안의 파일은 babel이 로드하지 않습니다.
-        include: [resolve('src'), resolve('test'), resolve('node_modules/tiptap'), resolve('node_modules/tiptap-extensions'), resolve('node_modules/prosemirror-tables'), resolve('node_modules/prosemirror-utils')]
+        include: [
+          resolve("src"),
+          resolve("test"),
+          resolve("node_modules/tiptap"),
+          resolve("node_modules/tiptap-extensions"),
+          resolve("node_modules/prosemirror-tables"),
+          resolve("node_modules/prosemirror-utils"),
+        ],
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
+          name: utils.assetsPath("img/[name].[hash:7].[ext]"),
+        },
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: utils.assetsPath('media/[name].[hash:7].[ext]')
-        }
+          name: utils.assetsPath("media/[name].[hash:7].[ext]"),
+        },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
-      }
-    ]
+          name: utils.assetsPath("fonts/[name].[hash:7].[ext]"),
+        },
+      },
+    ],
   },
   plugins: [
     new webpack.DllReferencePlugin({
       context: __dirname,
-      manifest: require('./vendor-manifest.json')
+      manifest: require("./vendor-manifest.json"),
     }),
     new HtmlWebpackIncludeAssetsPlugin({
       assets: [vendorAssets[0]],
-      files: ['index.html', 'admin/index.html'],
-      append: false
+      files: ["index.html", "admin/index.html"],
+      append: false,
     }),
-  ]
+  ],
 }
