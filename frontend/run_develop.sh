30c95acdbc935c6f0de3139f2b6cbbcfff61db13
Minchan Kim <kimmc3423@naver.com>
2026-09-09 10:55:22 +0900

    fix: run_develop.sh 조건문 문법 오류 수정

--- frontend/run_develop.sh
+++ frontend/run_develop.sh
@@ -3,7 +3,7 @@
 default_proxy="http://localhost:8000"
 export NODE_ENV=development
 
-if ["$1" == "--proxy"]; then
+if [ "$1" == "--proxy" ]; then
   proxy_ip="$2"
   echo "Proxy IP is $proxy_ip"
   export TARGET=$proxy_ip
